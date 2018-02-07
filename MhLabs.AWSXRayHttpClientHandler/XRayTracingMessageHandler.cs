cf7daf800c77ac9b760819d9dad1275ef06f5de5
Lars <lars@mathem.se>
2018-02-07 10:25:33 +0100

    first commit

---
+++ MhLabs.AWSXRayHttpClientHandler/XRayTracingMessageHandler.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.XRay.Recorder.Core;
+using Amazon.XRay.Recorder.Core.Internal.Entities;
+using Amazon.XRay.Recorder.Core.Internal.Utils;
+
+namespace MhLabs.AWSXRayHttpClientHandler
+{
+    public class XRayTracingMessageHandler : HttpClientHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            ProcessRequest(request);
+            HttpResponseMessage webResponse;
+            try
+            {
+                var responseAsync = await base.SendAsync(request, cancellationToken);
+                ProcessResponse(responseAsync);
+                webResponse = responseAsync;
+            }
+            catch (Exception ex)
+            {
+                AWSXRayRecorder.Instance.AddException(ex);
+                if (ex is HttpRequestException)
+                {
+                    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                    ProcessResponse(response);
+                }
+                throw;
+            }
+            finally
+            {
+                AWSXRayRecorder.Instance.EndSubsegment();
+            }
+            return webResponse;
+        }
+
+        private static void ProcessResponse(HttpResponseMessage response)
+        {
+            if (AWSXRayRecorder.Instance.IsTracingDisabled())
+                return;
+            var dictionary = new Dictionary<string, object>();
+            int statusCode = (int)response.StatusCode;
+            dictionary["status"] = (object)statusCode;
+            if (statusCode >= 400 && statusCode <= 499)
+            {
+                AWSXRayRecorder.Instance.MarkError();
+                if (statusCode == 429)
+                    AWSXRayRecorder.Instance.MarkThrottle();
+            }
+            else if (statusCode >= 500 && statusCode <= 599)
+                AWSXRayRecorder.Instance.MarkFault();
+            dictionary["content_length"] = (object)response.Content.Headers.ContentLength;
+            AWSXRayRecorder.Instance.AddHttpInformation(nameof(response), (object)dictionary);
+        }
+
+        private static void ProcessRequest(HttpRequestMessage request)
+        {
+            if (!AWSXRayRecorder.Instance.IsTracingDisabled())
+            {
+                AWSXRayRecorder.Instance.BeginSubsegment(request.RequestUri.Host);
+                AWSXRayRecorder.Instance.SetNamespace("remote");
+                var dictionary =
+                    new Dictionary<string, object>
+                    {
+                        ["url"] = request.RequestUri.AbsoluteUri,
+                        ["method"] = request.Method.Method
+                    };
+                AWSXRayRecorder.Instance.AddHttpInformation(nameof(request), dictionary);
+            }
+
+            if (!TraceHeader.TryParse(TraceContext.GetEntity(), out var header))
+                return;
+            request.Headers.Add("X-Amzn-Trace-Id", header.ToString());
+        }
+    }
+}
